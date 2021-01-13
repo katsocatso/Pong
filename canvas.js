7822b40e5e1af2c7fe017664377783ff56f32414
Catherine Wang <c622wang@uwaterloo.ca>
2021-01-12 23:41:58 -0500

    fixed window size

--- canvas.js
+++ canvas.js
@@ -1,2 +1,51 @@
 canvas = document.querySelector('#canvas');
 ctx = canvas.getContext('2d');
+
+function createCanvas(w, h) {
+    canvas.height = h;
+    canvas.width = w;
+    canvas.style = "position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin: auto";
+}
+
+function background() {
+    ctx.fillStyle = screenColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    //without first two lines, code compiles but has no effect
+
+    //drawing midline
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineWidth = 3;
+    ctx.setLineDash([12, 6]);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+}
+
+function displayStateMsg() {
+    if (gameState == "start") {
+        ctx.textAlign = "right";
+        ctx.fillStyle = objectsColor;
+        ctx.font = "60px Impact";
+        ctx.fillText("PONG", canvas.width / 2 - 100, canvas.height / 2);
+        ctx.font = "20px Impact";
+        ctx.fillText("Press Enter to start", canvas.width / 2 - 100, canvas.height / 2 + 60);
+    } else if (gameState == "end") {
+        ctx.fillStyle = objectsColor;
+        ctx.font = "20px Impact";
+        ctx.textAlign = "center";
+        if (aiPaddle.points == WIN_PTS) {
+            ctx.fillText("Player 2 Wins!", canvas.width / 2 + 100, canvas.height / 2 + 50);
+        } else {
+            ctx.fillText("Player 1 Wins!", canvas.width / 2 - 100, canvas.height / 2 + 50);
+        }
+    }
+}
+
+function displayScore() {
+    ctx.font = "50px Impact";
+    ctx.fillStyle = objectsColor;
+    ctx.textAlign = "center";
+    ctx.fillText(playerPaddle.points.toString(), canvas.width / 2 - 30, 60);
+    ctx.fillText(aiPaddle.points.toString(), canvas.width / 2 + 30, 60);
+}
--- pong.js
+++ pong.js
@@ -13,27 +13,6 @@ const WIN_PTS = 10;
 const screenColor = 'rgb(31, 32, 41)';
 const objectsColor = 'rgb(223, 225, 226)';
 
-function createCanvas(w, h) {
-    canvas.height = h;
-    canvas.width = w;
-    canvas.style = "position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; margin: auto";
-}
-
-function background() {
-    ctx.fillStyle = screenColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    //without first two lines, code compiles but has no effect
-
-    //drawing midline
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, 0);
-    ctx.lineWidth = 3;
-    ctx.setLineDash([12, 6]);
-    ctx.lineTo(canvas.width / 2, canvas.height);
-    ctx.strokeStyle = "white";
-    ctx.stroke();
-}
-
 function setup() {
     //createCanvas(window.innerWidth, window.innerHeight);
     createCanvas(700, 450);
@@ -42,35 +21,6 @@ function setup() {
     ball = new Ball();
     gameState = "start";
     servingPlayer = playerPaddle;
-}
-
-function displayStateMsg() {
-    if (gameState == "start") {
-        ctx.textAlign = "right";
-        ctx.fillStyle = objectsColor;
-        ctx.font = "60px Impact";
-        ctx.fillText("PONG", canvas.width / 2 - 100, canvas.height / 2);
-        ctx.font = "20px Impact";
-        ctx.fillText("Press Enter to start", canvas.width / 2 - 100, canvas.height / 2 + 60);
-    } else if (gameState == "end") {
-        ctx.fillStyle = objectsColor;
-        ctx.font = "20px Impact";
-        if (aiPaddle.points == WIN_PTS) {
-            //ctx.textAlign = "right";
-            ctx.fillText("Player 2 Wins!", canvas.width / 2 - 70, canvas.height / 2 + 50);
-        } else {
-            //ctx.textAlign = "left";
-            ctx.fillText("Player 1 Wins!", canvas.width / 2 - 70, canvas.height / 2 + 50);
-        }
-    }
-}
-
-function displayScore() {
-    ctx.font = "50px Impact";
-    ctx.fillStyle = objectsColor;
-    ctx.textAlign = "center";
-    ctx.fillText(playerPaddle.points.toString(), canvas.width / 2 - 30, 60);
-    ctx.fillText(aiPaddle.points.toString(), canvas.width / 2 + 30, 60);
 }
 
 function draw() {
@@ -89,7 +39,7 @@ function reset(servingPlayer) {
     ball.reset(servingPlayer, aiPaddle);
 }
 
-function update() {
+function update() {    
     let animID = window.requestAnimationFrame(update);
     if (gameState == "play") {
         ballUpdate();
